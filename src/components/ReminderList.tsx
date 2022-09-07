8782ced074ef49c6d6dba17e70c51d483c148248
Julian Florez <forezjuli08@hotmail.com>
2022-09-07 14:52:42 -0500

    modified, added button and changed styles

--- src/components/ReminderList.tsx
+++ src/components/ReminderList.tsx
@@ -3,13 +3,17 @@ import { Reminder } from '../models/Reminder'
 
 interface ReminderListProps {
   items: Reminder[]
+  onRemoveReminder: (id:number) => void
 }
 
-export default function ReminderList({ items }: ReminderListProps) {
+export default function ReminderList({ items, onRemoveReminder }: ReminderListProps) {
   return (
-    <ul className='max-w-2xl mx-auto text-white text-center'>
+    <ul className="max-w-2xl mx-auto text-white text-center">
       {items.map(item => (
-        <li className='mb-7 border-b-2 border-teal-200' key={item.id}>{item.title}</li>
+        <li className="flex justify-between items-center mb-7 border-b-2 border-teal-200" key={item.id}>
+          {item.title}
+          <button className="px-5 py-2 border-2 border-red-500 rounded-xl" onClick={()=>{onRemoveReminder(item.id)}}>Delete</button>
+        </li>
       ))}
     </ul>
   )
