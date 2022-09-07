0d7bd0660aa004a9f166cdd7dd3b4db90c08bc43
Julian Florez <forezjuli08@hotmail.com>
2022-09-07 14:40:50 -0500

    adding effect for getting the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReminderList from './components/ReminderList'
 import { Reminder } from './models/Reminder'
+import ReminderService from './services/Reminder'
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    {
-      id: 1,
-      title: 'Reminder 1',
-    },
-  ])
+  const [reminders, setReminders] = useState<Reminder[]>([])
+
+  const loadReminders = async () => {
+    const reminders = await ReminderService.getReminder()
+
+    setReminders(reminders)
+  }
+  useEffect(() => {
+    loadReminders()
+  }, [])
 
   return (
     <div className="w-full min-h-screen px-12 py-20 bg-slate-900">
